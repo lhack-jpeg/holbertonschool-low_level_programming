cf77ceb1009b9f3b39ecc8bd2d2b8eaaa44e14b0
Luke Hackett <luke-hackett@outlook.com>
2022-06-05 18:13:02 -0700

    feat: script to write to stderr

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  *main - Print to stderr
@@ -8,7 +9,9 @@
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 
-	return (0);
+	return (1);
 }
