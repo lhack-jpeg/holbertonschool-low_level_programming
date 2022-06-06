875d3bba894c16312df3ab7c1ab9e346125382b5
Luke Hackett <luke-hackett@outlook.com>
2022-06-05 19:39:04 -0700

    Fix: fix string structure

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,13 +16,19 @@ int main(void)
 	lastDigit = n % 10;
 	if (lastDigit < 6 && lastDigit != 0)
 	{
-		printf("%d and is less than 6 and not 0\n", lastDigit);
+		printf("Last of %d is %d and is less than 6 and not 0\n",
+		       n,
+		       lastDigit);
 	} else if (lastDigit > 5)
 	{
-		printf("%d and is greater than 5\n", lastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n",
+		       n,
+		       lastDigit);
 	} else
 	{
-		printf("%d and is zero\n", lastDigit);
+		printf("Last of %d is %d and is zero\n",
+		       n,
+		       lastDigit);
 	}
 	return (0);
 }
