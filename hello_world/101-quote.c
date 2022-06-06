b3d9018974d7328a94d05f98ed370ef4db8964e9
Luke Hackett <luke-hackett@outlook.com>
2022-06-05 18:03:46 -0700

    Feat: Script to print to stderr

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - Print to stderr
+ *
+ *Return: return 1 if success
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (0);
+}
