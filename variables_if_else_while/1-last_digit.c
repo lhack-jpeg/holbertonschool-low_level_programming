bce9a83a804c920c074bd9362c48e5fa35716b27
Luke Hackett <luke-hackett@outlook.com>
2022-06-05 19:41:22 -0700

    fix: fix typo

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 		       lastDigit);
 	} else
 	{
-		printf("Last digit of %d is %d and is zero\n",
+		printf("Last digit of %d is %d and is 0\n",
 		       n,
 		       lastDigit);
 	}
