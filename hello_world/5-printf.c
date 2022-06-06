145b110b77a6686a52c9ea89b654b6afe98e17fd
Luke Hackett <luke-hackett@outlook.com>
2022-06-05 17:22:28 -0700

    script to print using printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - print string using printf
+ *
+ * Return: return status
+ */
+
+int main(void)
+{
+	char str1[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", str1);
+
+	return (0);
+}
