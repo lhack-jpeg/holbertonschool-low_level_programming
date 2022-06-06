1b06f3c1947c08170bf990efc6d96cff37e782cb
Luke Hackett <luke-hackett@outlook.com>
2022-06-05 21:57:22 -0700

    Feat: script to print alphabet backwards

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - print alphabet backwards
+ *
+ *Return: always returns 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
