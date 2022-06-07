a7061d6b09cc70bf835b206db834e66e624a2541
Luke Hackett <luke-hackett@outlook.com>
2022-06-06 19:53:34 -0700

    feat: script to print _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Function to print _putchar
+ *
+ *Return: always returns 1
+ */
+
+int main(void)
+{
+	char str1[] = "_putchar";
+	int len = strlen(str);
+	int i;
+
+	for (i = 0; i < len; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
