99fb1951df68fcf1d891bb81072c29fa230cc781
Luke Hackett <luke-hackett@outlook.com>
2022-06-06 21:19:59 -0700

    Feat: Script to print alphabet 10x

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ *Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char ch;
+
+	i = 0;
+	while (i < 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+
+		_putchar('\n');
+
+		i++;
+	}
+}
