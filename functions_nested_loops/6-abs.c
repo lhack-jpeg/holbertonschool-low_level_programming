0b53eb5d72a40188ebce422472a6f0cd161f1df0
Luke Hackett <luke-hackett@outlook.com>
2022-06-06 22:22:28 -0700

    feat: function to return absolute value of integer

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_abs - Calculates the absolute value of the integer
+ *
+ *Return: returns the absolute value of the int
+ */
+
+int _abs(int j)
+{
+	int i = abs(j);
+
+	return (i);
+}
