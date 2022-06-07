2a9c68bceb7ba90aeab3eb4324d30f4201637a3b
Luke Hackett <luke-hackett@outlook.com>
2022-06-06 18:05:20 -0700

    Feat: Script to print out number combinations 3 digits

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ *main - print out the different combinations of integers
+ *
+ *Return: always returns 01
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	i = '0';
+	while (i < '7')
+	{
+		j = (i + 1);
+		while (j <= '8')
+		{
+			k = (j + 1);
+			while (k <= '9')
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i < '7')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
