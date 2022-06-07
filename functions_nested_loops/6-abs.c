2ed3e75ed5efab5928f5635a2bf38e3dd620e58d
Luke Hackett <luke-hackett@outlook.com>
2022-06-06 22:24:11 -0700

    fix: add docs on j char

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  *_abs - Calculates the absolute value of the integer
- *
+ *@j: is the integer being converted to absolute value
  *Return: returns the absolute value of the int
  */
 
