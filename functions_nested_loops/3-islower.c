c49c4a50f83ebf1023108d801e6684fc11699873
Luke Hackett <luke-hackett@outlook.com>
2022-06-06 21:37:29 -0700

    feat: func to return is char is lowercase

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+/**
+ *_islower - checks to see if c is lowercase
+ *
+ *Return: 1 if c is lower or 0 if not
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
