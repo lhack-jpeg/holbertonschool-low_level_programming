e1275bd81ef9b10d0ca41fabfac34e45bf6bc1c6
Luke Hackett <luke-hackett@outlook.com>
2022-06-09 23:01:43 -0700

    Script to print number combinations

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ *main - prints out total combinations for 0 - 9 in double combinations
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int a, i;
+
+	a = 0;
+	while (a < 99)
+	{
+		i = a + 1;
+		while (i < 100)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			if (a < 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
