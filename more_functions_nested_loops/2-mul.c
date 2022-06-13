5f549f9c1a53ad85acefb5fc774e76b39938f4ab
Luke Hackett <luke-hackett@outlook.com>
2022-06-12 21:30:54 -0700

    function to multiple two ints together

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * mul - returns the value of two ints multiplied together
+ * @a: one int to be multipled
+ * @b: second int to be multipled
+ *Return: the total of a * b
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
