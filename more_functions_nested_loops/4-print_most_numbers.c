4f14a08a0092328dafe83acb7e810e0e15fd08ba
Luke Hackett <luke-hackett@outlook.com>
2022-06-12 21:39:18 -0700

    function to print 0-9 skipping 2 & 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_most_numbers - prints 0 - 9 via _putchar while skipping 2 & 4
+ *
+ *Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = '0';
+	while (i <= '9')
+	{
+		if (i != '2' || i != '4')
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
