e938d84add9aefc833da615118160e581eba335a
Luke Hackett <luke-hackett@outlook.com>
2022-06-12 22:28:34 -0700

    function to print diagonally

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_diagonal - prints out '\' followed by '\n'
+ * lines are indented further with white space depending on size of n
+ *Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		i = 0;
+		/*run loop for value of n*/
+		while (i < n)
+		{
+			j = 0;
+			/*Loop to add more whitespace*/
+			while (j <= i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
