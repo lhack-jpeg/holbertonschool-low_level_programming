0bb238499d37a98b41ac1786c94e62bd7e79ba20
Luke Hackett <luke-hackett@outlook.com>
2022-06-12 22:37:33 -0700

    function to print out a square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - function to print a square of '#' dependant on 'size' using
+ * _putchar
+ * @size: value of size of 'square'
+ *Return: void
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		i = 0;
+		while (i < size)
+		{
+			j = 0;
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+}
