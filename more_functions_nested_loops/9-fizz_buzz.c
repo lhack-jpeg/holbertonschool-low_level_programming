340f3bb6cdf62f61e430b5659e4ef7cd0c09438a
Luke Hackett <luke-hackett@outlook.com>
2022-06-12 22:48:38 -0700

    fizz buzz c program

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *main - fizzbuzz, programs counts to 100 and prints of fizz if number is
+ * divisible by 3, prints out buzz if divisible by 5 and fizzbuzz if
+ * divisible by both
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i < 101)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
