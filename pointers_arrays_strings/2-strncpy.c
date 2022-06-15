f2ed8c58ca07d45952aa6d0bf964aac5c64c8a62
Luke Hackett <luke-hackett@outlook.com>
2022-06-15 02:14:15 -0700

    function to act like strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strncpy - copy the string from src for n elements and concatenate it onto
+ * dest
+ * @dest: dest pointer
+ * @src: src pointer
+ * @n: number of elements from src to copy over
+ *Return: char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	/*Iterates over each item in src string*/
+	for (i = 0; i < n && src[i] != '\0'; i ++)
+		dest[i] = src[i];
+	/*Adds a null character into the string if necessary*/
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
