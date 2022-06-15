560082c6cb217ca61625f36191a70c40bbf97c4b
Luke Hackett <luke-hackett@outlook.com>
2022-06-15 02:37:02 -0700

    Function to compare two strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,12 +12,12 @@ int _strcmp(char *s1, char *s2)
 	int value = 0, i = 0;
 
 	/*Continue loop while each char is the same value*/
-	while (*(s1 + i) == *(s2 + i))
+	while (*(s1 + i) == *(s2 + 1))
 	{
 		/*Break the loop if char is null*/
 		if (*(s1 + i) == '\0')
 			break;
-		if (*(s2+ i) == '\0')
+		if (*(s2 + i) == '\0')
 			break;
 		i++;
 	}
