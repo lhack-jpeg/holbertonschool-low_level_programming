208b8edc4d7cc9c503074c27f0e0768649fd8e59
Luke Hackett <luke-hackett@outlook.com>
2022-06-16 18:35:52 -0700

    Function to check if char from string exists

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * _strchr - returns a pointer the address of the char if found else null
+ * @s: the address to the string
+ * @c: the char we are searching for
+ *Return: the address or the null char
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	/*Loop until null terminator*/
+	while (s[i] != '\0')
+	{
+		if (c == s[i])
+		{
+			/*&s[i] sends back the remaining char from the address*/
+			return (&s[i]);
+		}
+		i++;
+	}
+	/*If char doesn't match any from array, return null term*/
+	return ('\0');
+}
