5ba9efd9c1d78f54a119a8dc85351812a7abe6b2
Luke Hackett <luke-hackett@outlook.com>
2022-06-16 18:00:18 -0700

    function to copy memory from address to another

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _memcpy - copy the string located at one memory address to another
+ * @dest: dest string address
+ * @src: source string address
+ * @n: amount of bytes to be copied
+ *Return: char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (*(src + i) != '\0' && i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	return (dest);
+}
