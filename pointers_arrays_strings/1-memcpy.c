30f65389c5c67780bdd4a6b8ea703ee1509196a8
Luke Hackett <luke-hackett@outlook.com>
2022-06-16 18:15:07 -0700

    function copy the string from one address to another

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -13,12 +13,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	i = 0;
-	while (*(src + i) != '\0' && i < n)
+	while (i < n)
 	{
 		*(dest + i) = *(src + i);
 		i++;
 	}
-	dest[i] = '\0';
-	
 	return (dest);
 }
