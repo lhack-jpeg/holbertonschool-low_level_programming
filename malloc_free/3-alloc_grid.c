da6226ecaa34b2b59a40162b1115b5fcb68f6924
Luke Hackett <luke-hackett@outlook.com>
2022-06-21 00:41:04 -0700

    Function to create array from ints given

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -24,14 +24,16 @@ int **alloc_grid(int width, int height)
 		for (i = 0; i < height; i++)
 		{
 			arr[i] = malloc(sizeof(*arr[i]) * width);
+
 			if (arr == NULL)
 			{
 				free(arr);
-				reutrn (NULL);
+				return (NULL);
 			}
+
 			for (j = 0; j < width; j++)
 				arr[i][j] = 0;
+		}
 	}
-
 	return (arr);
 }
