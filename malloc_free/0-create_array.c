998f9d5f57cdabc3829dbfea1ef529caeecf153b
Luke Hackett <luke-hackett@outlook.com>
2022-06-20 20:52:41 -0700

    Function to dynamically allocate memory to an array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - creates an array of char and initailises it
+ * with the specified char
+ * @size: size of array
+ * @c: first char of the array
+ *Return: char pointer or NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(size * sizeof(char));
+
+	ptr[0] = c;
+
+	return (ptr);
+}
