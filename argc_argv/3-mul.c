9e663a2274d0807eff64a839664bb9682e2f023a
Luke Hackett <luke-hackett@outlook.com>
2022-06-20 18:04:11 -0700

    Program to print the product of the argv

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - A program to print out the products of the args passed through on a
+ * new line
+ * @argc: the int value of the argv passed through
+ * @argv: the array of values
+ *Return: 0 if args are digits, 1 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, prod = 1;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		prod = prod * atoi(argv[i]);
+	}
+	printf("%d\n", prod);
+
+	return (0);
+}
