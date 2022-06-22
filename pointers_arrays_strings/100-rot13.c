6ae811acef87542f6b02088338e17fb1191c5e5e
Luke Hackett <luke-hackett@outlook.com>
2022-06-22 16:48:40 -0700

    Function convert alpha to rot13

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * rot13 - Encodes a string given in rot 13
+ * str - pointer to a string
+ *Return: pointer
+ */
+
+char *rot13(char *str)
+{
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	int i, j;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (alpha[j] != '\0')
+		{
+			if (str[i] == alpha[j])
+			{
+				str[i] = rot[j];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
