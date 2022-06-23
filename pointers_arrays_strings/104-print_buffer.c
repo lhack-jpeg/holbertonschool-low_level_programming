bba20997da4386b164d630677924807f99ef2b8f
Luke Hackett <luke-hackett@outlook.com>
2022-06-22 23:02:37 -0700

    revert back to old code

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -1,77 +1,86 @@
 #include <stdio.h>
-#include <ctype.h>
-/**
- * printable - check if a char is printable char
- * @c: char being check
- * Return: 1 if true, 0 otherwise
- */
-int printable(int c)
-{
-	if (isprint(c))
-		return (c);
+ #include <ctype.h>
+ /**
+  * printable - check if a char is printable char
+  * @c: char being check
+  * Return: 1 if true, 0 otherwise
+  */
+ int printable(int c)
+ {
+ 	if (isprint(c))
+ 		return (c);
 
-	return ('.');
-}
-/**
- * printhex - print the 2 place hex codes in lower case
- * @b: pointer to string
- * @count: line number
- * @size: size of the buffer for b
- * Return: void
- */
-void printhex(char *b, int count, int size)
-{
-	int i, right_pad_space;
+ 	return ('.');
+ }
+ /**
+  * printhex - print the 2 place hex codes in lower case
+  * @b: pointer to string
+  * @count: line number
+  * @size: size of the buffer for b
+  * Return: void
+  */
+ void printhex(char *b, int count, int size)
+ {
+ 	int i;
 
-	for (i = 0; i < 10; i += 2)
-	{
-		if (count + i >= size)
-			break;
+ 	for (i = 0; i < 10; i++)
+ 	{
+ 		if (size - i - count > 0)
+ 		{
+ 			printf("%02x", b[i + count]);
+ 			if (i % 2 == 1)
+ 				putchar(' ');
+ 		}
+ 		else
+ 		{
+ 			while (i < 10)
+ 			{
+ 				printf("  ");
+ 				if (i % 2 == 1)
+ 					putchar(' ');
+ 				i++;
+ 			}
+ 		}
+ 	}
+ }
+ /**
+  * printchar -  function to print char
+  * @b: string
+  * @count: int for line count
+  * @size: size of buffer
+  * Return: void
+  */
+ void printchar(char *b, int count, int size)
+ {
+ 	int i;
+ 	char ch;
 
-		printf("%02x%02x ", b[count + i], b[count + i + 1]);
-	}
-	right_pad_space = (10 - i) * 2 + ((10 - i) / 2);
-	if (right_pad_space > 0)
-	{
-		printf("%*c", right_pad_space, ' ');
-	}
-}
-/**
- * pointchar - function to print char
- * @b: string
- * @count: int for line count
- * @size: size of buffer
- * Return: void
- */
-void printchar(char *b, int count, int size)
-{
-	int i;
-	char ch;
+ 	for (i = 0; i < 10; i++)
+ 	{
+ 		if (size - i - count > 0)
+ 		{
+ 			ch = printable(b[i + count]);
+ 			printf("%c", ch);
+ 		}
+ 		else
+ 			break;
+ 	}
+ }
 
-	for (i = 0; i < 10; i++)
-	{
-		if (count + i > size)
-			break;
+ /**
+  * print_buffer - prints out the buffer and the string determined by
+  * the bytes passed through
+  * @b: the string passed through
+  * @size: the amount of bytes passed through
+  *Return: void
+  */
 
-		ch = printable(b[i + count]);
-		printf("%c", ch);
-	}
-}
-
-/**
- * print_buffer - prints out the buffer and the string determined by
- * the bytes passed through
- * @b: the string passed through
- * @size: the amount of bytes passed through
- *Return: void
- */
-
-void print_buffer(char *b, int size)
-{
-	int count = 0;
+ void print_buffer(char *b, int size)
+ {
+ 	int count = 0;
 
 	if (size == 0)
-		putchar('\n');
+		printf("\n");
 	else
 	{
 		while (count < size)
@@ -83,4 +92,4 @@ void print_buffer(char *b, int size)
 			count += 10;
 		}
 	}
-}
+ }
