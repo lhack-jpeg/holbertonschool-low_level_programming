faf3040266820af491279e8f444d5fd07b9e00a0
Luke Hackett <luke-hackett@outlook.com>
2022-06-27 18:41:29 -0700

    Create an arrray with min max values of arr

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_range - creates an array of integers with min and max
+ * @min: min value of int
+ * @max: max value of the array
+ *Return: pointer to the array
+ */
+
+int *array_range(int min, int max)
+{
+	int *array;
+	int size, i;
+
+	if (min > max)
+		return (NULL);
+	size = max - min + 1;
+
+	array = malloc(sizeof(int) * size);
+
+	i = 0;
+	while (i < size)
+	{
+		array[i] = min;
+		min++;
+		i++;
+	}
+	return (array);
+}
