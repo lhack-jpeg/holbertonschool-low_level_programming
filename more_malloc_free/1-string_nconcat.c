7bb7ec1edf17d1676aa490f4c0a154abe1eb90c0
Luke Hackett <luke-hackett@outlook.com>
2022-06-27 17:50:39 -0700

    Function to copy string with len n bytynes

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - returns the length of a string
+ *@s: pointer to the string
+ *Return: returns the string length
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+
+	while (s[length] != '\0')
+		length++;
+
+	return (length);
+}
+
+/**
+ * string_nconcat - concats two strings together
+ * @s1: pointer to str 1
+ * @s2: pointer to str 2
+ * @n: how bytes to use
+ *Return: pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *dest;
+	unsigned int i, j, str1len, str2len, totallen;
+
+	if (s1 == NULL)
+		str1len = 0;
+	else
+		str1len = _strlen(s1);
+	if (s2 == NULL)
+		str2len = 0;
+	else
+		str2len = _strlen(s2);
+	if (str2len > n)
+	{
+		str2len = n;
+	}
+	totallen = str1len + str2len;
+
+	dest = malloc(sizeof(*dest) * totallen + n);
+	if (dest == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < str1len)
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+
+	j = 0;
+	while (i < totallen)
+	{
+		dest[i] = s2[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
