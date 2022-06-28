f201c27749a4198df7c9540b15c4ca486cdd923b
Luke Hackett <luke-hackett@outlook.com>
2022-06-28 15:57:21 -0700

    header for structure of dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOGS_H_
+#define _DOGS_H_
+
+/**
+ * struct dog - structure for dog information
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*_DOG_H_*/
