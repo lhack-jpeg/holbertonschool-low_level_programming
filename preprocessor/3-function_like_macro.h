7b63998a9911264ac621da62b7ae557da7dbd526
Luke Hackett <luke-hackett@outlook.com>
2022-06-27 21:00:35 -0700

    Function macro to return absolute result

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,3 +1,4 @@
-#ifndef ABS
-#define ABS(X) ((X) >= 0 ? (X) : (X * -1))
-#endif /*ABS*/
+#ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
+#define ABS(X) ((X >= 0) ? (X) : (X * -1))
+#endif 
