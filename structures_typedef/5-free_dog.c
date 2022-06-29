c24eb9bb8665854d420413b9e5f2c644dbc245fa
Luke Hackett <luke-hackett@outlook.com>
2022-06-28 18:31:12 -0700

    Function to free malloc from struct

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - function to free the struct1
+ * @d: pointer to the struct
+ *Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	/*free(d->age);*/
+	free(d->owner);
+	free(d);
+}
