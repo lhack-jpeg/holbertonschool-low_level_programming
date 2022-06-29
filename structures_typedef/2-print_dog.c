8ab754669aa293342c0c57f1e5c7fbf40a5b69df
Luke Hackett <luke-hackett@outlook.com>
2022-06-28 17:22:44 -0700

    Changed nil print on age

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -18,7 +18,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", d->name);
 		if (!(d->age))
-			printf("Age: %f\n", 0);
+			printf("Age: 0.0000n");
 		else
 			printf("Age: %f\n", d->age);
 		if (d->owner == NULL)
