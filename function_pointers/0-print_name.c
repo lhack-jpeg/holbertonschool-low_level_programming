1b0f34826f8b0e953cc0621ee7270876e66d239b
Luke Hackett <luke-hackett@outlook.com>
2022-06-28 21:00:16 -0700

    Function to pass function and name through

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * print_name - prints a name
+ * @name: pointer to the name
+ * @f: pointer the function
+ *Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
