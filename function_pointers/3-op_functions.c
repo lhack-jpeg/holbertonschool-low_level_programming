de7a97d21b0f43a07641300eaa57765325d288a6
Luke Hackett <luke-hackett@outlook.com>
2022-06-29 23:19:39 -0700

    Add check for 0

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
 /**
  * op_add - returns sum of a + b
@@ -44,6 +46,11 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
@@ -56,5 +63,11 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	return (a % b);
 }
