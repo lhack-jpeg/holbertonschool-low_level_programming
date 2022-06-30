2c07f9c893dd89782e3be7808f8e7b4a2863eb57
Luke Hackett <luke-hackett@outlook.com>
2022-06-29 20:34:16 -0700

    Function to task 3

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * get_op_func - returns the op_function
+ * @s: pointer to string (argv)
+ * Return: integer
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (i < 5)
+	{
+		if (*(ops[i].op) == *s)
+		{
+			return (ops[i].f);
+		}
+		printf("i: %d\n", i);
+		i++;
+	}
+
+	printf("This is the argv passed through: %s\n", s);
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+/**
+ * op_add - returns sum of a + b
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns difference of a & b
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns product of a & b
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns division of a & b
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns sum of a + b
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
