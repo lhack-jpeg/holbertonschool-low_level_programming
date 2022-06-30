99f41d22508312bf4d4627d1dc2ef4c06d93fad8
Luke Hackett <luke-hackett@outlook.com>
2022-06-29 20:35:57 -0700

    Removed printf

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -16,8 +16,8 @@ int main(__attribute__ ((unused)) int argc, char *argv[])
 	s = argv[2];
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	printf("%s: argv[2]\n", s);
-	printf("Num1: %d, num2: %d\n", num1, num2);
+	/* printf("%s: argv[2]\n", s); */
+	/* printf("Num1: %d, num2: %d\n", num1, num2); */
 	result = get_op_func(s)(num1, num2);
 	printf("Result: %d\n", result);
 
