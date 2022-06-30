53a9652c514d5ffd6b0a08ae8337b6d9ef996f3c
Luke Hackett <luke-hackett@outlook.com>
2022-06-29 23:19:28 -0700

    Add checks for error

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -13,12 +13,23 @@ int main(__attribute__ ((unused)) int argc, char *argv[])
 	int num1, num2, result;
 	char *s;
 
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	s = argv[2];
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	/* printf("%s: argv[2]\n", s); */
 	/* printf("Num1: %d, num2: %d\n", num1, num2); */
 	result = get_op_func(s)(num1, num2);
+
+	if (!result)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	printf("%d\n", result);
 
 	return (0);
