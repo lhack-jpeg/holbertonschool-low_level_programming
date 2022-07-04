8e9772ef75f9d4db2ce194a0a5222bf5c4a5795d
Luke Hackett <luke-hackett@outlook.com>
2022-07-04 02:08:51 -0700

    Function to print list of strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_strings - prints new strings followed by new line
+ * @separator - separator between strings
+ * @n: count of args
+ *Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ptr;
+	unsigned int count;
+	char *sep = "", *temp_word;
+
+	if (separator != NULL)
+	{
+		sep = strdup(separator);
+	}
+	va_start(ptr, n);
+	count = 0;
+	while (count < n)
+	{
+		temp_word = va_arg(ptr, char *);
+		if (temp_word)
+		{
+			printf("%s", temp_word);
+		}
+		else
+		{
+			printf("(nil)");
+		}
+		if (count < n - 1)
+			printf("%s", sep);
+		count++;
+	}
+	printf("\n");
+	va_end(ptr);
+}
