57217936c21e038124577f2d93c9da68def09a74
Luke Hackett <luke-hackett@outlook.com>
2022-07-04 02:55:50 -0700

    Add proto and struct for prrint all

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,15 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
+#include <stdarg.h>
+typedef struct my_print
+{
+	char *c;
+	void (*f)(va_list arg);
+} my_print;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif /* _VARIADIC_FUNCTIONS_H_ */
