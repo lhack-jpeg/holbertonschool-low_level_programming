d46db12961f4272b0e3931620a12ccbab71d2650
Luke Hackett <luke-hackett@outlook.com>
2022-07-04 00:54:56 -0700

    Function to print numbers with sep

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+/**
+ * print_numbers - prints numbers with a separator
+ * @separator: pointer to separator
+ * @n: number of args to be passed
+ *Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ptr;
+	unsigned int count;
+	char *sep = "";
+
+	if (separator != NULL)
+	{
+		sep = strdup(separator);
+	}
+
+	va_start(ptr, n);
+	count = 0;
+	while (count < n)
+	{
+		printf("%d%s", va_arg(ptr, int), sep);
+		count++;
+	}
+	printf("\n");
+	va_end(ptr);
+}
