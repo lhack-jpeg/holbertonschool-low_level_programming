b2d44da51ea1f495ee12f2c53b8b13e488fc5b86
Luke Hackett <luke-hackett@outlook.com>
2022-07-11 22:07:33 -0700

    function to print elements of array

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - prints the elements of a struct list
+ * @h: pointer to struct list item
+ *Return: size_t of number of elements
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("[%d]\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
