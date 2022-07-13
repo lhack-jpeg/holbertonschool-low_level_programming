a1967823711283b07408eec38f919f48e5011013
Luke Hackett <luke-hackett@outlook.com>
2022-07-12 23:37:57 -0700

    function to insert node at index

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+/**
+ * listint_len - counts number of items in linked list listint_t
+ * @h: pointer to struct list item
+ *Return: size_t of number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
+
+/**
+ * insert_nodeint_at_index - insert node at index specified
+ * @head: pointer to pointer for first value of list
+ * @idx: index to insert new node
+ * @n: int to be stored in new node
+ *Return: pointer to new node
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int len;
+	listint_t *new, *prev, *temp;
+
+	len = listint_len(*head);
+	if (len < idx)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+
+	prev = *head;
+	while (idx > 1)
+	{
+		prev = prev->next;
+		idx--;
+	}
+	temp = prev->next;
+	prev->next = new;
+	new->next = temp;
+
+	return (new);
+}
