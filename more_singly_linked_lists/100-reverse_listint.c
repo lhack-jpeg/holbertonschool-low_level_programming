d97cadfc377350ab38cfa1e020a005c0d0e2b6ea
Luke Hackett <luke-hackett@outlook.com>
2022-07-14 21:12:06 -0700

    changed to recursion

--- more_singly_linked_lists/100-reverse_listint.c
+++ more_singly_linked_lists/100-reverse_listint.c
@@ -1,6 +1,31 @@
 #include "lists.h"
 
 /**
+ * reverse_list - reverse a linked list
+ * @curr: pointer to current node of list
+ * @prev: previous node
+ * @head: pointer to start of list
+ *Return: pointer to new start of list
+ */
+
+listint_t *reverse_list(listint_t *curr, listint_t *prev, listint_t **head)
+{
+	listint_t *next;
+
+	if (!curr->next)
+	{
+		*head = curr;
+		curr->next = prev;
+		return (*head);
+	}
+
+	next = curr->next;
+	curr->next = prev;
+	reverse_list(next, curr, head);
+	return (*head);
+}
+
+/**
  * reverse_listint - reverse a linked list
  * @head: pointer to start of list
  *Return: pointer to new start of list
@@ -8,28 +33,9 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev, *temp;
-
 	if (head == NULL || *head == NULL)
 		return (NULL);
 
-	if ((*head)->next == NULL)
-		return (*head);
-
-	prev = *head;
-	temp = prev->next;
-	*head = temp->next;
-	prev->next = NULL;
-
-	while ((*head)->next != NULL)
-	{
-		temp->next = prev;
-		prev = temp;
-		temp = *head;
-		*head = (*head)->next;
-	}
-
-	(*head)->next = temp;
-	temp->next = prev;
+	*head = reverse_list(*head, NULL, head);
 	return (*head);
 }
