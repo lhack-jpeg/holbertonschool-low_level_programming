f8fb27da44fd359b47aa23cf27b8a8d4ed114076
Luke Hackett <luke-hackett@outlook.com>
2022-07-18 22:50:55 -0700

    Chnaged condition on NULL

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -61,7 +61,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		str = "";
+		text_content = "";
 
 	len = _strlen(text_content);
 	str = malloc(sizeof(*str) * len + 1);
