a595cacd5b3bdf58e38e0c0145bd2971e6449462
Luke Hackett <luke-hackett@outlook.com>
2022-07-18 17:42:22 -0700

    function to convert str of binary to unsigned int

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert string of 0 & 1 to unsigned int
+ * @b: pointer to string
+ *Return: 0 if b is NULL or contains char that is greater than 1 else return int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0, mult = 1;
+	int len;
+
+	if (b == NULL)
+		return (0);
+
+	len = strlen(b) - 1;
+
+	while (b[len])
+	{
+
+		if (b[len] >= '2')
+			return (0);
+
+		if (b[len] == '1')
+		{
+			num += 1 * mult;
+			mult *= 2;
+		}
+
+		if (b[len] == '0')
+		{
+			mult *= 2;
+		}
+		len--;
+	}
+
+	return num;
+}
