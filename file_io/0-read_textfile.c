ded3a93424c0f9c416bb32135656b47c3f5d3303
Luke Hackett <luke-hackett@outlook.com>
2022-07-18 22:03:12 -0700

    function to print file

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX
+ * standard output
+ * @filename: pointer to file name
+ * @letters: buffer size of letters
+ * Return: actual numbers of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+        ssize_t filedes, filewrite, fileread;
+	char *stored;
+
+	if (filename == NULL)
+		return (0);
+
+	filedes = open(filename, O_RDONLY);
+	if (filedes == -1)
+		return (0);
+
+	stored = malloc(sizeof(*stored) * letters);
+	if (stored == NULL)
+		return (0);
+
+	fileread = read(filedes, stored, letters);
+	if (fileread == -1)
+	{
+		free(stored);
+		return (0);
+	}
+
+	filewrite = write(STDOUT_FILENO, stored, letters);
+	if (filewrite == -1)
+		return (0);
+
+	free(stored);
+	close(filedes);
+	return (fileread);
+}
---
+++ file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stddef.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef long int ssize_t;
+
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif /* _MAIN_H_ */
