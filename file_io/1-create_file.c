204302ec8055cbda1da9bfb9d5a68a05404a08f1
Luke Hackett <luke-hackett@outlook.com>
2022-07-18 22:47:33 -0700

    Add flag for trunc

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -56,7 +56,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	filedes = open(filename, O_CREAT | O_RDWR, 0600);
+	filedes = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (filedes == -1)
 		return (-1);
 
