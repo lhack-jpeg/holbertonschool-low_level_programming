ad7c1dd81d57ce8ec5d102586cbd98ac98df269b
Luke Hackett <luke-hackett@outlook.com>
2022-07-19 22:35:43 -0700

    function print list from head pointer and return count

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,21 @@
-#include <stdio.h>
+#include "lists.h"
+
 /**
- *main -
- *
- *Return:
+ * print_dlistint - function that prints all the elements of a dlistint_t list
+ * @h: const pointer to start of double linked list
+ *Return: size of list
  */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
