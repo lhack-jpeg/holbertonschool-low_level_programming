b9eabece0bf960b7bbcf1bc465961e21cbd06f8b
Luke Hackett <luke-hackett@outlook.com>
2022-07-21 16:52:08 -0700

    fix: change nodebefore-prev in idx catch

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -49,6 +49,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new->next = nodebefore;
 		new->prev = NULL;
+		nodebefore->prev = new;
 		*h = new;
 		return (new);
 	}
