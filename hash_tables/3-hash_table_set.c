23d0eaba49e40f72bc5045f2c438619b0cfc85d8
Luke Hackett <luke-hackett@outlook.com>
2022-08-17 17:27:07 -0700

    Update function to add node beginning of linked list

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -34,7 +34,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
-		ht->array[index]->next = node;
+		node->next = ht->array[index]
+		ht->array[index] = node;
 	}
 
 	return (1);
