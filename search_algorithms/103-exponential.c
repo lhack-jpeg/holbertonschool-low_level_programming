2fd31a9a3fa4cae58e6edc9d43e078b46e65993d
Luke Hackett <luke-hackett@outlook.com>
2022-08-25 19:53:45 -0700

    function to search array using exponential methjod

---
+++ search_algorithms/103-exponential.c
@@ -0,0 +1,71 @@
+#include "search_algos.h"
+
+/**
+ * b_search - binary search through a sorted array
+ * @array: pointer to first element in the array
+ * @low: left side of array
+ * @high: right side of array
+ * @value: value to search for
+ *Return: index of value or -1 otherwise
+ */
+
+int b_search(int *array, int low, int high, int value)
+{
+	int idx, mid;
+
+	if (array == NULL)
+		return (-1);
+
+	while (high >= low)
+	{
+		printf("Searching in array: ");
+		for (idx = low; idx < high; idx++)
+			printf("%d, ", array[idx]);
+		printf("%d\n", array[idx]);
+
+		mid = low + (high - low) / 2;
+
+		if (value == array[mid])
+			return (mid);
+
+		if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+	return (-1);
+}
+
+/**
+ * exponential_search - search through a sorted array using an exponential step
+ * search algorithm
+ * @array: pointer to first element in array
+ * @size: size/length of array
+ * @value: value to search for
+ * Return: index of value , -1 otherwise
+ */
+
+int exponential_search(int *array, size_t size, int value)
+{
+	int idx;
+	size_t step = 1;
+
+	if (array == NULL)
+		return (-1);
+
+	while (step < size)
+	{
+		if (array[step] >= value)
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n",
+			       step / 2, step);
+			idx = b_search(array, step / 2, step - 1, value);
+			return (idx);
+		}
+		printf("Value check array[%ld] = [%d]\n", step, array[step]);
+		step *= 2;
+	}
+	printf("Value found between indexes [%ld] and [%ld]\n", step / 2, step);
+	idx = b_search(array, step / 2, step - 1, value);
+	return (idx);
+}
